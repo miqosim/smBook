db937889a42c1d59d7a1f5c8893647164d748548
miqosim <simonyanmiqayel@gmail.com>
2017-11-01 21:40:54 +0400

    Added some folders

---
+++ app/src/main/java/com/softwareengineering/smbook/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.softwareengineering.smbook.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softwareengineering.smbook.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
